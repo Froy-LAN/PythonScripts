9ddc2ce5e2df09debfc62f57b23b66afbf29f089
Froy-LAN <97267751+Froy-LAN@users.noreply.github.com>
2022-01-21 19:12:30 -0500

    Update log_analyzer.py

--- log_analyzer/log_analyzer.py
+++ log_analyzer/log_analyzer.py
@@ -2,65 +2,62 @@
 import re
 import operator
 import csv
-from collections import OrderedDict #used instead of a dictionary as it is not ordered in some versions of Pytho
+from collections import OrderedDict    #imported in case environment does default to ordered dictionaries
 
-error = {}          #dictionary to retain error messages and the number of each
-per_user = {}       #dictionary to contain how many INFO or ERROR log entries there are per user
-log = []            #list to inster lines from log into
 
-with open('syslog.log') as f:    #read lines from log
+error = {}
+per_user = {}
+
+
+with open('syslog.log') as f:       #read entries from log file titled 'syslog.log'
     log = f.readlines()
 
-for line in log:                 #get rid of any space at either end of each line
-    log[log.index(line)] = line.strip()
+for line in log:
+    log[log.index(line)] = line.strip()    #remove any unnecessary spaces at either end of the line
 
 
-pattern_err = r"ticky: ERROR ([\w \']*) \(([\w.]*)\)"
-pattern_inf = r"ticky: INFO ([a-zA-Z ]*) \[#\d*\] \((\w.*)\)"
+pattern = r"(ERROR|INFO) ([ 'a-zA-Z]*)\[?#?\d?\d?\d?\d?\]? \(([\w.]*)\)" #pattern that recognizes entry type, content message, and username
 
-for line in log:                        #used to distinguish ERROR vs INFO entries on the log
-    if re.search(pattern_err, line):
+for line in log:
 
-        x = re.search(pattern_err, line)          #if it is an ERROR entry, add the message to the corresponding dictionary
-                                                  
-        if x.group(1) not in error:
-            error[x.group(1)] = 1
+    x = re.search(pattern, line)        #extract the pattern groups from each entry
+    if x.group(1) == "ERROR":           #if the entry is an ERROR entry, perform the following
+
+        if x.group(2) not in error:       #Increase count of error type
+            error[x.group(2)] = 1
         else:
-            error[x.group(1)] += 1
+            error[x.group(2)] += 1
 
-        if x.group(2) not in per_user:           #increase the number of ERROR entires per user 
-            per_user[x.group(2)] = [0, 1]
+        if x.group(3) not in per_user:     #increase the number of error entries per user
+            per_user[x.group(3)] = [0, 1]
         else:
-            per_user[x.group(2)] = [per_user.get(x.group(2))[0], per_user.get(x.group(2))[1] + 1]
+            per_user[x.group(3)] = [per_user.get(x.group(3))[0], per_user.get(x.group(3))[1] + 1]
 
-    elif re.search(pattern_inf, line):          #if it is an INFO entry, do not add the message to the ERROR dictionary
+    else:  #if the entry is an INFO entry, perform the following
 
-        y = re.search(pattern_inf, line)
-
-        if y.group(2) not in per_user:          #increase the number of INFO entries per user
-            per_user[y.group(2)] = [1, 0]
+        if x.group(3) not in per_user:       #increase the number of info type entries per user
+            per_user[x.group(3)] = [1, 0]
         else:
-            per_user[y.group(2)] = [per_user.get(y.group(2))[0] + 1, per_user.get(y.group(2))[1]]
+            per_user[x.group(3)] = [per_user.get(x.group(3))[0] + 1, per_user.get(x.group(3))[1]]
 
 
-sorted_error = OrderedDict()             #create orderd dictionary objects 
-sorted_user = OrderedDict()
+sorted_error = {}     #create ordered dictionary objects so all entries retain their placement
+sorted_user = {}
 
-for item in (sorted(per_user.items(), key=operator.itemgetter(0))):       #move objects from unordered dictionaries to ordered ones
-    sorted_user[item[0]] = item[1]
+for item in (sorted(per_user.items(), key=operator.itemgetter(0))): #sort dictionary with number of each type of entry (INFO|ERRO)
+    sorted_user[item[0]] = item[1]                                  #to be ordered alphabetically
 
-for item in (sorted(error.items(), key=operator.itemgetter(1), reverse=True)):
-    sorted_error[item[0]] = item[1]
+for item in (sorted(error.items(), key=operator.itemgetter(1), reverse=True)): #sort dictionary with count of error type to be sorted
+    sorted_error[item[0]] = item[1]                                            #from the highest to lowest number
 
-
-with open('user_statistics.csv', 'w') as file:                        #create csv files to hold the processed log information as stored 
-    writer = csv.writer(file)                                         #in the ordered dictionaries
+with open('user_statistics.csv', 'w', newline='') as file:                                #create filed called 'user_statistics.csv'
+    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)    #add number of entries per type to it
     writer.writerow(["Username", "INFO", "ERROR"])
     for key, value in sorted_user.items():
         writer.writerow([key, value[0], value[1]])
 
-with open('error_message.csv', 'w') as file:
-    writer = csv.writer(file)
+with open('error_message.csv', 'w', newline='') as file:                                  #create file calld 'error_messages.csv'
+    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)    #add number error type 
     writer.writerow(["Error", "Count"])
     for key, value in sorted_error.items():
         writer.writerow([key, value])
