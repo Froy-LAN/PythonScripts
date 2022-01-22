d178de21f89187423d689dfcf31a8b392a98cfc2
Froy-LAN <97267751+Froy-LAN@users.noreply.github.com>
2022-01-21 20:28:41 -0500

    Update backup.py

--- backup/backup.py
+++ backup/backup.py
@@ -2,24 +2,24 @@
 
 import subprocess
 import os
-import tarfile
 from multiprocessing import Pool
-from os.path import join
+
 
 src = "./main/"
 dest = "./backup/"
 directories = []
 
-for root, dirs, files in os.walk(src, topdown=False):
+for root, dirs, files in os.walk(src, topdown=False):  #list the names of the direcories found in the directory named 'main'
    for name in dirs:
-        directories.append(name)
+        directories.append(name)                       #add directories to a list
 
 def sync_dir(directory):
+   ''' uses the Linux tool rsync to backup files from a specified source to a specified destination'''
         subprocess.call(["rsync", "-zrvh", os.path.join(src, directory),  dest] )
         source = os.path.join(src, directory)
         destination = os.path.join(dest, directory)
-        print("Backing up folder {} and its contents into {}".format(directory, destination))
+        print("Backing up folder {} and its contents into {}".format(directory, destination)) #print output to let users know what directories are being backed up
 
 
-p = Pool(len(directories))
-p.map(sync_dir, directories)
+p = Pool(len(directories))     #create a pool of tasks to be completed
+p.map(sync_dir, directories)   #backup each subdirectory of the 'main' directory and process each as a separate thread
