a58bd4a92817c481aed1608fa94b1c5eb5143456
Froy-LAN <97267751+Froy-LAN@users.noreply.github.com>
2022-01-21 20:16:55 -0500

    Update server.py

--- socket_encrypt/server.py
+++ socket_encrypt/server.py
@@ -6,16 +6,16 @@ from cryptography.fernet import Fernet
 
 message = "You are connected to the server"
 
-encryption_key = Fernet.generate_key()
-fernet = Fernet(encryption_key)
+encryption_key = Fernet.generate_key()   #generate encryption key
+fernet = Fernet(encryption_key)          
 
-encrypted_message = fernet.encrypt(message.encode())
+encrypted_message = fernet.encrypt(message.encode())  #encrypt message
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 7777)) #the consecutive single quotes ('') can be replaced be socket.gethostname() if both files are tested on the same host
 s.listen()
 
 while True:
-	clientsocket, IP = s.accept()
-	print("Client with IP address {} has connected ".format(IP[0]))
-	clientsocket.send(encrypted_message)
+	clientsocket, IP = s.accept()                         #accept connection from client
+	print("Client with IP address {} has connected ".format(IP[0]))   #print IP address of connecting client
+	clientsocket.send(encrypted_message)                   #send message
